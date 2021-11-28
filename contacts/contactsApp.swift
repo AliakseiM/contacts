7cdb653e6c4910bb7c256b91c9db7651afbcca8c
Alexey Mihailovsky <alexey.mihailovsky@gmail.com>
2021-11-28 16:28:52 +0300

    Initial Commit

---
+++ contacts/contactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  contactsApp.swift
+//  contacts
+//
+//  Created by Алексей Михайловский on 28.11.21.
+//
+
+import SwiftUI
+
+@main
+struct contactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
